2ea9e5c6efa6c51ca8ad3255dad2eec83797ab9e
Harshit Srivastava <101548705+gem-harshitsrivastava@users.noreply.github.com>
2025-02-22 02:08:57 +0530

    feature: adder for numbers 0,1,n

---
+++ calculator.js
@@ -0,0 +1,6 @@
+export const calculator=(...nums)=>{
+    let sum=0
+    for(let n of nums)
+        sum+=n
+    return sum
+}
---
+++ test/calculator.spec.js
@@ -0,0 +1,22 @@
+import { calculator } from "../calculator.js";
+import { assert } from "chai";
+
+describe('calculator',()=>{
+    it("should return 0 for no input",()=>{
+        const result = calculator();
+        assert.equal(result,0)
+    })
+    it('should return the number when 1 input is provided',()=>{
+        const result = calculator(5);
+        assert.equal(result,5)
+    })
+    it('should add up the numbers when 2 input are provided',()=>{
+        const result = calculator(1,2);
+        assert.equal(result,3)
+    })
+    it('should return sum of as many number provided',()=>{
+        const result = calculator(1,2,3,4,5,5)
+        assert.equal(result,20)
+    })    
+
+})
